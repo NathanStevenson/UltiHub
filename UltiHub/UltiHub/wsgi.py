f8fc43d36236ab318c78040e23e815c6974e9490
Nathan <nts7bcj@virginia.edu>
2023-06-16 16:48:00 -0700

    debugging deploying

--- UltiHub/UltiHub/wsgi.py
+++ UltiHub/UltiHub/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UltiHub.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UltiHub.settings')
+
 app = get_wsgi_application()
